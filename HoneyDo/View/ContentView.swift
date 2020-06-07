6f89ee6bf279c2a487eca7fcb391bc172cd382ae
adamjohnlea <imnogeekdptcom@me.com>
2020-06-07 14:02:09 -0600

    add animated image and text if todo list is empty

--- HoneyDo/View/ContentView.swift
+++ HoneyDo/View/ContentView.swift
@@ -21,29 +21,35 @@ struct ContentView: View {
   
   var body: some View {
     NavigationView {
-      List{
-        ForEach(self.todos, id: \.self) {todo in
-          HStack {
-            Text(todo.name ?? "Unknown")
-            Spacer()
-            Text(todo.priority ?? "Uknown")
-          }
-        }//: FOREACH
-        .onDelete(perform: deleteTodo)
-      }//: LIST
-        .navigationBarTitle("Todo", displayMode: .inline)
-        .navigationBarItems(
-          leading: EditButton(),
-          trailing:
-          Button(action: {
-            self.showingAddTodoView.toggle()
-          }) {
-            Image(systemName: "plus")
-          }//: ADD BUTTON
-            .sheet(isPresented: $showingAddTodoView) {
-              AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
-          }
+      ZStack {
+        List{
+          ForEach(self.todos, id: \.self) {todo in
+            HStack {
+              Text(todo.name ?? "Unknown")
+              Spacer()
+              Text(todo.priority ?? "Uknown")
+            }
+          }//: FOREACH
+          .onDelete(perform: deleteTodo)
+        }//: LIST
+          .navigationBarTitle("Todo", displayMode: .inline)
+          .navigationBarItems(
+            leading: EditButton(),
+            trailing:
+            Button(action: {
+              self.showingAddTodoView.toggle()
+            }) {
+              Image(systemName: "plus")
+            }//: ADD BUTTON
+              .sheet(isPresented: $showingAddTodoView) {
+                AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
+            }
         )
+        // MARK: - NO TODO ITEMS
+        if todos.count == 0 {
+          EmptyListView()
+        }
+      }//: ZSTACK
     }//: NAVIGATION VIEW
   }
   
